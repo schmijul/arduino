23f9dec99474c70a63f034e17762e351a7b3958b
JulianSchmidt96 <69909913+JulianSchmidt96@users.noreply.github.com>
2021-06-21 11:59:03 +0200

    Add files via upload

--- start_all.sh
+++ start_all.sh
@@ -1,33 +1,55 @@
 #!/bin/bash
-
-
-sh ltestart.sh
+green=`tput setaf 2`
+reset=`tput sgr0`
+sudo sh ltestop.sh
+sudo sh ltestart.sh
 sleep 2
-echo "lte-connection established "
+echo "${green} lte-connection established  ${reset}"
 pidof sh ltesart.sh
 
 sh gps.sh &
 sleep 2
-echo "gps-meassurement started "
-gps=$pidof (sh gps.sh)
+echo "${green} gps-meassurement started  ${reset}"
+gps=$(pidof -s sh gps.sh)
 
 sh ping.sh &
+sleep 2 
+echo "${green} ping-meassurement started ${reset}"
+ping=$(pidof -s sh ping.sh)
+
+
+
+
+
+gnome-terminal -- python3 monitor-mobileinsight-and-timefix.py /dev/ttyUSB0 9600
 sleep 2
-echo "ping-meassurement started "
-ping=$(pidof sh ping.sh)
+echo "${green} mobileinsight-meassurement started ${reset} "
+mobileinsight=$(pidof -s python3 monitor-mobileinsight-and-timefix.py)
 
-python3 monitor-mobileinsight-and-timefix.py /dev/ttyUSB0 9600 &
+
+sh tcpdump.sh &
+
+pcap=$(pidof -s tcpdump)
+
+echo "${green}  choose iperf_meassurement ${reset}"
+
+read a
+
+cd iperf_scripts/testdrive_2/
+
+
+gnome-terminal -- python3 iperf_scheduler.py $a
 sleep 2
-echo "mobileinsight-meassurement started "
-mobileinsight=$(pidof python3 monitor-mobileinsight-and-timefix.py /dev/ttyUSB0 9600)
+echo "${green} iperf-meassurement $1 started  ${reset}"
+iperf=$(pidof -s python3 iperf_scripts/iperf_scheduler.py $a)
+
+sleep 8
+echo {$gps,$ping,$pcap,$mobileinsight,$iperf}
 
 
 
+sleep 3
+watch ps -p {$gps,$ping,$pcap,$mobileinsight,$iperf}
 
-gnome-terminal --tab --title="iperf" --command="bash -c 'python3 iperf_scripts/iperf_scheduler.py $1 &'i"
-sleep 2
-echo"iperf-meassurement $1 started "
-iperf=$(pidof python3 iperf_scripts/iperf_scheduler.py $1)
 
-watch ps -p {$gps,$ping,$mobileinsight,$iperf}
 
